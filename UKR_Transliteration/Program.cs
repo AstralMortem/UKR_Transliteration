f944d6503b021fb1859ea086131a24875ff73ec9
Vladyslav Chaliuk <vladchaluk@gmail.com>
2023-05-09 15:17:28 +0300

    Добавьте файлы проекта.

---
+++ UKR_Transliteration/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Transliteration {
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Transliteration.Translate("Готиня"));
+        }
+    }
+
+    public static class Transliteration
+    {
+        static Dictionary<string, string> table_char = new Dictionary<string, string>() {
+            {"а", "a" },
+            {"б","b" },
+            {"в","v" },
+            {"г","h" },
+            {"д","d" },
+            {"е","e" },
+            {"є","ie" },
+            {"ж","zh" },
+            {"з","z" },
+            {"и","y" },
+            {"і","i" },
+            {"ї","i" },
+            {"й","i" },
+            {"к","k" },
+            {"л","l" },
+            {"м","m" },
+            {"н","n" },
+            {"о","o" },
+            {"п","p" },
+            {"р","r" },
+            {"с","s" },
+            {"т","t" },
+            {"у","u" },
+            {"ф","f" },
+            {"х","kh" },
+            {"ц","ts" },
+            {"ч","ch" },
+            {"ш","sh" },
+            {"щ","shch" },
+            {"ю","iu" },
+            {"я","ia" },
+        };
+
+        static Dictionary<string, string> extra_table_char = new Dictionary<string, string>() {
+
+            {"є", "ye" },
+            {"ї", "yi" },
+            {"ю", "yu" },
+            {"я", "ya" },
+        };
+
+        public static string Translate(string str)
+        {
+            string result ="";
+            string ch = "";
+
+            for(int i=0; i< str.Length; i++)
+            {
+                
+                if(i == 0)
+                {
+                    if (!extra_table_char.TryGetValue(str[i].ToString().ToLower(), out ch))
+                    {
+                        table_char.TryGetValue(str[i].ToString().ToLower(), out ch);
+                    }
+
+                    if (Char.IsUpper(str[i]))
+                    {
+                        ch = FirstCharToUpper(ch);
+                    }
+                    result += ch;
+                }
+                else
+                {
+                    
+                    table_char.TryGetValue(str[i].ToString(), out ch);
+                    result += ch;
+                }
+
+
+            }
+
+            return result;
+        }
+        public static string FirstCharToUpper(string input)
+        {
+            string res = "";
+            for(int i=0; i< input.Length ; i++)
+            {
+                if (i == 0)
+                {
+                    res += Char.ToUpper(input[i]);
+                    continue;
+                }
+                res += input[i];
+            }
+            return res;
+        }
+
+        
+
+
+    }
+
+
+
+
+}
